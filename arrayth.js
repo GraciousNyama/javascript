db1dbe82aa227a9ba0d754d50d3a9655fef37949
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-04 12:00:25 +0200

    pop  and push on array
    used push and pop to add and or subtract words

--- arrayth.js
+++ arrayth.js
@@ -1,5 +1,10 @@
-var things =[23 ," cars", " Harvey", 11];
- things.push("twelve");
- 
-document.getElementById("demo").innerHTML= "<ul>" + "<li>"+ things[2] + "</li>" + "<li>" + "has" + "</li>"  + "<li>"
-+ things[4]  + "</li>" + "<li>" + things[1]+ "</li>" + "</ul>";
+var cars =["BMW" ," Benz", " Toyota"];
+
+document.getElementById("demo").innerHTML="there  are" + cars.length +"cars ...."+ cars.toString();
+cars.push("land rover");
+
+document.getElementById("memo").innerHTML="there  are now" + cars.length +"cars ..."+cars.toString();
+
+cars.pop();
+cars.pop();
+document.getElementById("log").innerHTML="there  are now " + cars.length +"cars ..."+cars.toString();
