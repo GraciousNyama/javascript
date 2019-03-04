d884758296ebbebc6953e3bfb7ae9c3e782c4251
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-04 11:10:06 +0200

    push on array
    using push array on javascript

---
+++ arrayth.js
@@ -0,0 +1,5 @@
+var things =[23 ," cars", " Harvey", 11];
+ things.push("twelve");
+ 
+document.getElementById("demo").innerHTML= "<ul>" + "<li>"+ things[2] + "</li>" + "<li>" + "has" + "</li>"  + "<li>"
++ things[4]  + "</li>" + "<li>" + things[1]+ "</li>" + "</ul>";
