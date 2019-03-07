e44a1d2d88df29ca90592936e8e9f06ebba0ae2f
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-07 09:28:08 +0200

    if statement
    used if and else tp dispaly true or false statements

--- forloopfs.js
+++ forloopfs.js
@@ -1,6 +1,17 @@
 var days= ["Monday", "Tuesday", "Wednesday"];
 
-if(days[0] == "Monday"){document.write("Hey i found Monday");
+if(days[0] == "Monday"){
+document.write("Hey i found Monday <br>");
 }else{
-  document.write("Bummer man, no Monday here");
+document.write("Bummer man, no Monday here <br>");
+}
+ if (days[1] == "Tuesday"){
+   document.write("Hey i found Tuesday <br>");
+}else{
+document.write("Bummer man, no Tuesday here <br>")
+}
+ if(days[2] == "Wednesday"){
+   document.write(" Hey i found Wednesday <br>");
+}else{
+ document.write("Bummer man, no wednesday here <br>");
 }
