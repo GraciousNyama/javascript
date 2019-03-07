16391d9bc53b259e0cf8084a38c71a3d8b10afb3
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-07 09:44:22 +0200

    switch condition
    used switch statements to display true statement

---
+++ switch.js
@@ -0,0 +1,14 @@
+var cars =["BMW","Land Rover","skorokoro","lexus"]
+switch(cars[2]){
+  case "Land Rover":
+  document.write("Land Rovers are here <br>");
+  break;
+  case "lexus":
+  document.write("lexus are here <br>");
+  break;
+  case "skorokoro":
+  document.write("skorokoro is here <br>");
+  break;
+  default:
+  document.write("No lexus or land rover <br>");
+}
