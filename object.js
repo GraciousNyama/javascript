2bc7b2966100e444879159558bff17ced08d841c
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-11 11:43:47 +0200

    loop revision
    used for loop to create a statement  using elements of objects and array

--- object.js
+++ object.js
@@ -1,11 +1,22 @@
 
-var car ={
-  brand:" Toyota",
-  make:"  Corolla",
-  type:"  Sedan",
-  color:" blue",
+var corolla ={
+  brand:"Toyota",
+  make:"Corolla",
+  type:"Sedan",
+  color:"blue",
   yearofRelease:1997
 };
-document.getElementById("demo").innerHTML="this is a" + car.brand + "car called a" +car.make;
-document.getElementById("memo").innerHTML="it is a" + car.type + "which is"
-+ car.color + "and was released in" + car.yearofRelease;
+var rangeRover={
+  brand:"Range Rover",
+  make:"Sport",
+  type:"Suv",
+  color:"white",
+  yearofRelease:2010
+};
+var cars=[corolla, rangeRover];
+for(i=0; i<cars.length; i++){
+  document.write(
+    "this is a " + cars[i].brand + " car called a" + cars[i].make +
+    "!<br> it is a" + cars[i].type + " which is" + cars[i].color +
+    " and was released in" + cars[i].yearofRelease + "<br/><br/>");
+  }
