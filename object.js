d82bb554525e5553f1a8ee6de5ab769254d21a84
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-11 10:25:55 +0200

    objects
    used object element to create statements

---
+++ object.js
@@ -0,0 +1,11 @@
+
+var car ={
+  brand:" Toyota",
+  make:"  Corolla",
+  type:"  Sedan",
+  color:" blue",
+  yearofRelease:1997
+};
+document.getElementById("demo").innerHTML="this is a" + car.brand + "car called a" +car.make;
+document.getElementById("memo").innerHTML="it is a" + car.type + "which is"
++ car.color + "and was released in" + car.yearofRelease;
