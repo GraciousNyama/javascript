70dd998a8cce358292e40a452895bb524329055a
Gracious Nyamagoromondo <nyamagoromondogracious@gmail.com>
2019-03-11 09:07:05 +0200

    slidedown method
    used slidedown method   to flip the panel

--- jquery.js
+++ jquery.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
-  $("button.button").dblclick(function(){
-    $(".weare").hide();
-});
-});
-
-$(document).ready(function(){
-  $("ul li:first").dblclick(function(){
-    $("ul li:first").hide();
-});
-});
-
-$(document).ready(function(){
-  $("button.show").dblclick(function(){
-    $("ul li:first,.weare").show()
-});
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
 });
